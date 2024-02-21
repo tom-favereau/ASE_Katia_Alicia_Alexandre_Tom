e8080114eb7ce0e68d3b06918efb0dff50e9658f
[afaureve] <[tom.favereautpe@gmail.com]>
2024-02-21 08:59:20 +0100

    first commit

---
+++ src/test/java/com/project/ase_project/AseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ase_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
