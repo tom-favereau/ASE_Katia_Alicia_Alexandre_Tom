dfb52cc99b77254c496ce2ae7a4b0918329d1cf7
DepInfoAliciaSHAO <alicia.shao@depinfonancy.net>
2024-03-13 11:30:57 +0100

    fix removed unnecessary files

--- src/main/java/com/project/ase_project/AseProjectApplication.java
+++ src/main/java/com/project/ase_project/AseProjectApplication.java
@@ -3,13 +3,6 @@ package com.project.ase_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.project.ase_project.repository.SummonerRepo;
-import com.project.ase_project.model.Summoner;
-
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class AseProjectApplication {
@@ -17,14 +10,4 @@ public class AseProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(AseProjectApplication.class, args);
     }
-
-    @Bean
-    CommandLineRunner dataLoader(SummonerRepo repository) {
-        return args -> {
-            repository.save(new Summoner(null, "azerty", "mordeckaiLovuur"));
-            repository.save(new Summoner(null, "qwerty", "reneeRaps"));
-            repository.save(new Summoner(null, "keysmash", "revolutioN_"));
-        };
-    }
-
 }
--- src/main/java/com/project/ase_project/repository/SummonerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.ase_project.repository;
-import com.project.ase_project.model.Summoner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SummonerRepo extends JpaRepository<Summoner, Long> {
-    Summoner findByPpuid(String ppuid);
-    Summoner findByUsername(String username);
-}
-
