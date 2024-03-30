36b405757562fb9a5b867eb1e2697b3102a1b147
DepInfoAliciaSHAO <alicia.shao@depinfonancy.net>
2024-03-30 17:39:52 +0100

    add documentation

--- src/main/java/com/project/ase_project/controller/MapController.java
+++ src/main/java/com/project/ase_project/controller/MapController.java
@@ -2,6 +2,8 @@ package com.project.ase_project.controller;
 
 import com.project.ase_project.model.ddragon.maps.LOLMap;
 import com.project.ase_project.service.MapService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +18,15 @@ public class MapController {
     @Autowired
     private MapService service;
     @GetMapping("/maps")
+    @Tag(name = "Maps", description = "Methods for Map APIs")
+    @Operation(summary = "Get the list of all maps", description = "Get the list of all current existing maps.")
     public List<LOLMap> findAllMaps(){
         return service.getMaps();
     }
     @GetMapping("/maps/{id}")
+    @Tag(name = "Maps")
+    @Operation(summary = "Get a map's info", description = "Get a map's info given by mapId: mapId, name and associated " +
+            "notes.")
     public LOLMap findMapById(@PathVariable Integer id){
         return service.getMapById(id);
     }
--- src/main/java/com/project/ase_project/controller/QueueController.java
+++ src/main/java/com/project/ase_project/controller/QueueController.java
@@ -2,6 +2,8 @@ package com.project.ase_project.controller;
 
 import com.project.ase_project.model.ddragon.queue.LOLQueue;
 import com.project.ase_project.service.QueueService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +18,15 @@ public class QueueController {
     @Autowired
     private QueueService service;
     @GetMapping("/queues")
+    @Tag(name = "Queues", description = "Methods for Queue APIs")
+    @Operation(summary = "Get the list of all queues", description = "Get the list of all current existing queues.")
     public List<LOLQueue> findAllQueues(){
         return service.getQueues();
     }
     @GetMapping("/queues/{id}")
+    @Tag(name = "Queues")
+    @Operation(summary = "Get a map's info", description = "Get a map's info given by queueId: queueId, associated map " +
+            "and description.")
     public LOLQueue findQueueById(@PathVariable Integer id){
         return service.getQueueById(id);
     }
