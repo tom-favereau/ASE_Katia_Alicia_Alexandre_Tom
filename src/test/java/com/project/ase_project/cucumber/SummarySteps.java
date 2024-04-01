ebf984dda1489b290b34f7bdea9622321caa0ab7
DepInfoAliciaSHAO <alicia.shao@depinfonancy.net>
2024-04-01 22:23:15 +0200

    add cucumber coverage for summary feature

--- src/test/java/com/project/ase_project/cucumber/SummarySteps.java
+++ src/test/java/com/project/ase_project/cucumber/SummarySteps.java
@@ -1,58 +1,114 @@
 package com.project.ase_project.cucumber;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.project.ase_project.AseProjectApplication;
+
 import com.project.ase_project.model.clean.summary.Summary;
-import com.project.ase_project.model.ddragon.maps.LOLMap;
+import com.project.ase_project.model.clean.summoner.Summoner;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-
-
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class SummarySteps extends CucumberSpringConfiguration {
     private ResponseEntity<Summary> beluga;
-    @When("^the user calls /riot/summary/Belugafurtif$")
-    public void the_user_issues_GET_summary() {
-        String url = "http://localhost:8080/riot/summary/Belugafurtif";
-        //belugaSummary = new RestTemplate().getForObject(url, Summary.class);
+    private ResponseEntity<Summary> razork;
+    private ResponseEntity<String> errorNotFound;
+    private ResponseEntity<String> errorBadRequest;
+
+    ResponseEntity<Summary> getResponseEntity(String url){
+        String fullUrl = "http://localhost:8080" + url;
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        beluga = restTemplate.exchange(url, HttpMethod.GET, entity, Summary.class);
+        return restTemplate.exchange(fullUrl, HttpMethod.GET, entity, Summary.class);
+    }
+    @When("the user calls {string}")
+    public void theUserCalls(String url) {
+        if (url.contains("Belugafurtif")){
+            beluga = getResponseEntity(url);
+        } else if (url.contains("Razørk Activoo")) {
+            razork = getResponseEntity(url);
+        }
     }
 
-    @Then("the user receives summoner summary")
-    public void theUserReceivesSummonerSummary() {
-        Summary belugaSummary = beluga.getBody();
-        assertEquals("F4btU20wCQOmkMlWn4QJm33f3jH-B5Nj-uPfNnyuLED3PT0DpQ_LLcB_IQ", belugaSummary.getSummonerId());
-        assertEquals("Belugafurtif", belugaSummary.getSummonerName());
-        assertNotNull(belugaSummary.getSummonerLevel());
-        assertNotNull(belugaSummary.getProfileIconId());
-        assertNotNull(belugaSummary.getRankFlex());
-        assertNotNull(belugaSummary.getRankSolo());
-        assertNotNull(belugaSummary.getRegion());
-        assertNotNull(belugaSummary.getAverage());
-        assertNotNull(belugaSummary.getCardinal());
-        assertNotNull(belugaSummary.getProfileIconAddress());
+    @Then("the user receives {string}'s summary")
+    public void theUserReceivesSSummary(String summoner) {
+        if (summoner.equals("Belugafurtif")) {
+            Summary belugaSummary = beluga.getBody();
+            assertEquals("F4btU20wCQOmkMlWn4QJm33f3jH-B5Nj-uPfNnyuLED3PT0DpQ_LLcB_IQ", belugaSummary.getSummonerId());
+            assertEquals("Belugafurtif", belugaSummary.getSummonerName());
+            assertNotNull(belugaSummary.getSummonerLevel());
+            assertNotNull(belugaSummary.getProfileIconId());
+            assertNotNull(belugaSummary.getRankFlex());
+            assertNotNull(belugaSummary.getRankSolo());
+            assertNotNull(belugaSummary.getRegion());
+            assertNotNull(belugaSummary.getAverage());
+            assertNotNull(belugaSummary.getCardinal());
+            assertNotNull(belugaSummary.getProfileIconAddress());
+        } else if (summoner.equals("Razørk Activoo")) {
+            Summary belugaSummary = razork.getBody();
+            assertEquals("Dno8J85K7DTwnqSlFm64Nk_681yWe3BFSWsDuLJ5ayxpRhA", belugaSummary.getSummonerId());
+            assertEquals("Razørk Activoo", belugaSummary.getSummonerName());
+            assertNotNull(belugaSummary.getSummonerLevel());
+            assertNotNull(belugaSummary.getProfileIconId());
+            assertNotNull(belugaSummary.getRankFlex());
+            assertNotNull(belugaSummary.getRankSolo());
+            assertNotNull(belugaSummary.getRegion());
+            assertNotNull(belugaSummary.getAverage());
+            assertNotNull(belugaSummary.getCardinal());
+            assertNotNull(belugaSummary.getProfileIconAddress());
+        }
     }
 
-    @And("the response code status is {int}")
-    public void theResponseCodeStatusIs(int arg0) {
-        assertEquals(arg0, beluga.getStatusCode().value());
+    @And("the response code status for {string} is {int}")
+    public void theResponseCodeStatusForIs(String summoner, int code) {
+        if (summoner.equals("Belugafurtif")) {
+            assertEquals(code, beluga.getStatusCode().value());
+        } else if (summoner.equals("Razørk Activoo")) {
+            assertEquals(code, razork.getStatusCode().value());
+        }
+    }
+
+    @When("a GET request is made to the endpoint {string} \\(where A does not exist)")
+    public void aGETRequestIsMadeToTheEndpointWhereADoesNotExist(String url) {
+        try {
+            getResponseEntity(url);
+        } catch (HttpClientErrorException ex) {
+            errorNotFound = new ResponseEntity<>(ex.getResponseBodyAsString(), ex.getStatusCode());
+        }
+    }
+
+    @Then("the API should respond with a status code {int} \\(Not Found)")
+    public void theAPIShouldRespondWithAStatusCodeNotFound(int code) {
+        assertEquals(code, errorNotFound.getStatusCode().value());
+    }
+
+    @And("the response body should contain an error message indicating that the summoner was not found")
+    public void theResponseBodyShouldContainAnErrorMessageIndicatingThatTheSummonerWasNotFound() {
+        assertEquals("Erreur 404 : Le joueur A n'existe pas.", errorNotFound.getBody());
+    }
+
+    @When("an invalid request is made to the endpoint {string} \\(where the summoner Name is missing)")
+    public void anInvalidRequestIsMadeToTheEndpointWhereTheSummonerNameIsMissing(String url) {
+        try {
+            getResponseEntity(url);
+        } catch (HttpClientErrorException ex) {
+            errorBadRequest = new ResponseEntity<>(ex.getResponseBodyAsString(), ex.getStatusCode());
+        }
+    }
+
+    @Then("the API should respond with a status code {int} \\(Bad Request)")
+    public void theAPIShouldRespondWithAStatusCodeBadRequest(int code) {
+        assertEquals(code, errorBadRequest.getStatusCode().value());
+    }
+
+    @And("the response body should contain an error message indicating that the request is invalid")
+    public void theResponseBodyShouldContainAnErrorMessageIndicatingThatTheRequestIsInvalid() {
+        assertEquals("Erreur 400 : Veuillez préciser un pseudo de joueur.", errorBadRequest.getBody());
     }
 }
--- src/test/resources/com/project/ase_project/cucumber/summary.feature
+++ src/test/resources/com/project/ase_project/cucumber/summary.feature
@@ -1,5 +1,24 @@
 Feature: the summary can be retrieved
+  #Happy Path 1
   Scenario: user makes call to GET /riot/summary/Belugafurtif
-    When the user calls /riot/summary/Belugafurtif
-    Then the user receives summoner summary
-    And the response code status is 200
+    When the user calls "/riot/summary/Belugafurtif"
+    Then the user receives "Belugafurtif"'s summary
+    And the response code status for "Belugafurtif" is 200
+
+  #Happy Path 2
+  Scenario: user makes call to GET /riot/summary/Razørk Activoo
+    When the user calls "/riot/summary/Razørk Activoo"
+    Then the user receives "Razørk Activoo"'s summary
+    And the response code status for "Razørk Activoo" is 200
+
+  #Sad Path 1
+  Scenario: user makes call to GET /riot/summary/A (Summoner does not exist)
+    When a GET request is made to the endpoint "/riot/summary/A" (where A does not exist)
+    Then the API should respond with a status code 404 (Not Found)
+    And the response body should contain an error message indicating that the summoner was not found
+
+  #Sad Path 2
+  Scenario: user makes call to GET /riot/summary/ (Summoner is missing)
+    When an invalid request is made to the endpoint "/riot/summary/" (where the summoner Name is missing)
+    Then the API should respond with a status code 400 (Bad Request)
+    And the response body should contain an error message indicating that the request is invalid
