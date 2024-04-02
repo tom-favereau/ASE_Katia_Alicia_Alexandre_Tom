a43551b3704304666bc59a2ba033b72b519198e2
aethiiis <pubalexpub@gmail.com>
2024-04-02 23:46:29 +0200

    retrait d'une annotation parasite

--- src/test/java/com/project/ase_project/cucumber/CucumberSpringConfiguration.java
+++ src/test/java/com/project/ase_project/cucumber/CucumberSpringConfiguration.java
@@ -2,13 +2,10 @@ package com.project.ase_project.cucumber;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import jakarta.annotation.PostConstruct;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @CucumberContextConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = TestConfig.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class CucumberSpringConfiguration {
 
